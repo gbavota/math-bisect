5b78895c9f04edcda44224c67e9802bc253cf8da
gbavota <gabriele.bavota@gmail.com>
2018-05-18 15:22:31 +0200

    commented first method

--- src/repository/MathFunctions.java
+++ src/repository/MathFunctions.java
@@ -2,18 +2,19 @@ package repository;
 
 public class MathFunctions {
 
+	/*Computes the factorial of a number*/
 	public static int factorial(int n){
 		if(n<=1)
 			return 1;
 		else
 			return n*factorial((n-1));
 	}
-	
-	
+
+
 	public static double computeCircleArea(double radius){
 		return Math.PI * radius * radius;
 	}
-	
-	
-	
+
+
+
 }
